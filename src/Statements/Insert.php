b480aac5fd694bdc9b14029593d141aae9b827a7
Eric Siegel <esiegel@buymybreaker.com>
2025-09-09 12:00:49 -0400

    Clean up `INSERT` statement

--- src/Statements/Insert.php
+++ src/Statements/Insert.php
@@ -6,52 +6,38 @@
 
 	use CzProject\SqlGenerator\Helpers;
 	use CzProject\SqlGenerator\IDriver;
-	use CzProject\SqlGenerator\IStatement;
+    use CzProject\SqlGenerator\InvalidArgumentException;
+    use CzProject\SqlGenerator\IStatement;
 	use CzProject\SqlGenerator\TableName;
-
 
 	class Insert implements IStatement
 	{
-		/** @var string|TableName */
-		private $tableName;
+		private TableName|string $tableName;
 
 		/** @var array<string, mixed> */
-		private $data;
-
+		private array $data;
 
 		/**
-		 * @param  string|TableName $tableName
-		 * @param  array<string, mixed> $data
+		 * @param array<string, mixed> $data
 		 */
-		public function __construct($tableName, array $data)
+		public function __construct(string|TableName $tableName, array $data)
 		{
 			$this->tableName = Helpers::createTableName($tableName);
 			$this->data = $data;
 		}
 
+        /**
+         * @throws InvalidArgumentException
+         */
+        public function toSql(IDriver $driver): string {
+            try {
+                $tableName = Helpers::escapeTableName($this->tableName, $driver);
+                $fields = implode(',', array_map(static fn (string $field) => $driver->escapeIdentifier($field), array_keys($this->data)));
+                $values = implode(',', array_map(static fn (mixed $value) => Helpers::formatValue($value, $driver), array_values($this->data)));
 
-		public function toSql(IDriver $driver)
-		{
-			$output = 'INSERT INTO ' . Helpers::escapeTableName($this->tableName, $driver);
-
-			// columns
-			$output .= ' (';
-			$output .= implode(', ', array_map([$driver, 'escapeIdentifier'], array_keys($this->data)));
-			$output .= ")\nVALUES (";
-
-			// data
-			$fields = count($this->data);
-
-			foreach ($this->data as $value) {
-				$output .= Helpers::formatValue($value, $driver);
-				$fields--;
-
-				if ($fields > 0) {
-					$output .= ', ';
-				}
-			}
-
-			$output .= ');';
-			return $output;
+                return "INSERT INTO $tableName($fields) VALUES ($values);";
+            } catch (InvalidArgumentException $e) {
+                throw new InvalidArgumentException('Error creating UPDATE query.', previous: $e);
+            }
 		}
 	}
